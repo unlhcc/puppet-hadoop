57501a0b4b303df736c95f6972b9d462af91372f
John Thiltges <jthiltges2@unl.edu>
2018-12-20 12:17:14 -0600

    When looking for /hadoop-data* directories, return all, even if the mount point has failed

--- lib/facter/hdfs_dfsdir_list.rb
+++ lib/facter/hdfs_dfsdir_list.rb
@@ -7,7 +7,14 @@
 
 Facter.add("hdfs_dfsdir_list") do
 	setcode do
-		Dir['/hadoop-data*'].join(',')
+		#Dir['/hadoop-data*'].join(',')
+		# Ruby calls stat() on entries and doesn't return them if the
+		# call fails. We want to keep failed mount points in the list.
+		Dir.entries('/').
+			sort.
+			select { |str| str.start_with?("hadoop-data") }.
+			map { |str| 'file:///' + str }.
+			join(',')
 	end
 end
 
