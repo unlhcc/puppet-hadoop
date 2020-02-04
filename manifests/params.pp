6aec6d2062d364351db1e9fe4df9d83d2adae464
Garhan Attebury <garhan.attebury@unl.edu>
2020-02-04 11:17:41 -0600

    adjust default datanode heap based on system ram amount

--- manifests/params.pp
+++ manifests/params.pp
@@ -3,7 +3,16 @@
 class hadoop::params {
 
     $datanode_service_enable = false
-    $datanode_heap_size = 1024
+
+    # datanodes with less than 70GB RAM get 1024MB heap by default
+    # datanodes with more get 4096MB heap by default
+    if $facts['memory']['system']['total_bytes'] < 75161927680 {
+        $datanode_heap_size = 1024
+    }
+    else {
+        $datanode_heap_size = 4096
+    }
+
     $ganglia_udp_send_channel = undef
     $namenode_service_enable = false
 
